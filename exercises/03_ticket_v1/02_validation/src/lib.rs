26417455c90c6bb69a6ca0ec1cd49106e635ea53
farbian_2005 <fabian_bernhart@web.de>
2026-09-08 23:06:23 +0200

    learming From trait

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,7 +1,26 @@
 struct Ticket {
     title: String,
     description: String,
-    status: String,
+    status: Status,
+}
+
+enum Status {
+    Done,
+    InProgress,
+    Todo,
+}
+
+impl From<&str> for Status {
+    /// Converts a `&str` into a [`Status`].
+    #[inline]
+    fn from(s: &str) -> Status {
+        match s {
+            "To-Do" => Status::Todo,
+            "Done" => Status::Done,
+            "In Progress" => Status::InProgress,
+            _ => panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed"),
+        }
+    }
 }
 
 impl Ticket {
@@ -18,7 +37,24 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title.is_empty() {
+            panic!("Title cannot be empty")
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty")
+        }
+
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes")
+        }
+
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes")
+        }
+
+        let status = Status::from(status.as_str());
+
         Self {
             title,
             description,
